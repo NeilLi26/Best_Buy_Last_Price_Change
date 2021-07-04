749bb45d757e8a6e24b15df683dc3640b756c5b9
neilli01 <neilli01@student.ubc.ca>
2021-07-04 11:36:36 -0600

    first completed version of project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Best_Buy_Last_Price_Change
+
+I have been spending a lot of time window shopping on best buy lately, and a big problem for me is that
+I almost always only buy things when they are on sale. Yet some items almost never go on sale, and best buy does
+not show this information. Luckily, best buy's API has this information for me to check it with.
--- src/websiteAction.js
+++ src/websiteAction.js
@@ -1,52 +1,104 @@
 window.onload = function() {
   let check = document.querySelector('#check');
   check.addEventListener('click', function() {
-    document.querySelector('#search').value;
+    APIKeyword = document.querySelector('#search').value;
 
-    products = document.querySelector('#products')
-
-    createTabs(products);
+    initialiseAPI(generateAPIString(APIKeyword))
   })
+}
 
-  function createTabs(products){
-    cardCon = document.createElement("div")
-    rowCon = document.createElement("div")
-    leftCon = document.createElement("div")
-    cardImg = document.createElement("img")
-    rightCon = document.createElement("div")
-    cardBody = document.createElement("div")
-    cardHeader = document.createElement("h5")
-    cardText = document.createElement("p")
+function createTabs(productsCon, imgLink, prodcutName, productPrice, productReview, productLastPriceUpdate){
+  cardCon = document.createElement("div")
+  rowCon = document.createElement("div")
+  leftCon = document.createElement("div")
+  cardImg = document.createElement("img")
+  rightCon = document.createElement("div")
+  cardBody = document.createElement("div")
+  cardHeader = document.createElement("h5")
+  cardPrice = document.createElement("p")
+  cardReview = document.createElement("p")
+  cardLastPriceUpdate = document.createElement("p")
 
-    cardCon.className = "card text-info border-info mb-3"
-    rowCon.className = "row no-gutters"
-    leftCon.className = "col-md-4"
-    cardImg.className = "card-img"
-    rightCon.className = "col-md-8"
-    cardBody.className = "card-body"
-    cardHeader.className = "card-title"
-    cardText.className = "card-text"
+  cardCon.className = "card text-info border-info mb-3"
+  rowCon.className = "row no-gutters"
+  leftCon.className = "col-md-4"
+  cardImg.className = "card-img"
+  rightCon.className = "col-md-8"
+  cardBody.className = "card-body"
+  cardHeader.className = "card-title"
+  cardPrice.className = "card-text"
+  cardReview.className = "card-text"
+  cardLastPriceUpdate.className = "card-text"
 
-    cardCon.style = "max-width: 1080px;"
+  cardCon.style = "max-width: 1080px;"
 
-    cardImg.src = "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6403/6403425_sa.jpg"
-    cardImg.alt = "failed to load"
+  cardImg.src = imgLink
+  cardImg.alt = "failed to load"
 
-    cardHeader.innerText = "Card title"
-    cardText.innerText = "Some example text some example text. John Doe is an architect and engineer"
-    products.appendChild(cardCon)
-    cardCon.appendChild(rowCon)
-    rowCon.appendChild(leftCon)
-    rowCon.appendChild(rightCon)
-    leftCon.appendChild(cardImg)
-    rightCon.appendChild(cardBody)
-    cardBody.appendChild(cardHeader)
-    cardBody.appendChild(cardText)
-    
-  }
+  cardHeader.innerText = prodcutName
+  cardPrice.innerText = "Price: $" + productPrice + "USD"
+  cardReview.innerText = "Customer review: " + productReview
+  cardLastPriceUpdate.innerText = "price last updated on: " + productLastPriceUpdate
+
+  productsCon.appendChild(cardCon)
+  cardCon.appendChild(rowCon)
+  rowCon.appendChild(leftCon)
+  rowCon.appendChild(rightCon)
+  leftCon.appendChild(cardImg)
+  rightCon.appendChild(cardBody)
+  cardBody.appendChild(cardHeader)
+  cardBody.appendChild(cardPrice)
+  cardBody.appendChild(cardReview)
+  cardBody.appendChild(cardLastPriceUpdate)
+  
 }
 
 function generateAPIString(keyWord) {
   return "https://api.bestbuy.com/v1/products((search=" + keyWord
-  +"))?apiKey=VFCoW7Ikv64nAhD4KxmuxF9A&sort=bestSellingRank.dsc&show=bestSellingRank,name,image,salePrice,customerReviewAverage,priceUpdateDate&format=json"
+  +"))?apiKey=VFCoW7Ikv64nAhD4KxmuxF9A&sort=bestSellingRank.dsc&show=bestSellingRank,name,image,regularPrice,salePrice,customerReviewAverage,priceUpdateDate&format=json"
+}
+
+//MODIFIES: this
+//EFFECTS: given the API, get the arrays filled
+function initialiseAPI (API) {
+  fetch(API)
+    .then(response =>  response.json())
+      .then(data => data.products)
+        .then(
+          products => generateTabsFromProducts(products)
+          )
+  // .catch(error => console.log('Error'))
+}
+
+//MODIFIES: this
+//EFFECTS: given a list of producst in JSON form, create tabs with useful information
+function generateTabsFromProducts(products) {
+  console.log(products);
+
+  var productsCon = document.querySelector('#products')
+
+  document.body.removeChild(productsCon)
+  
+  var newProducts = document.createElement("div")
+  newProducts.id = "products"
+  document.body.appendChild(newProducts)
+
+  products.forEach(element => {
+    // numArray.push(Number(element.high));
+    // numArray.push(Number(element.low));
+    console.log(element.customerReviewAverage)
+    console.log(element.name)
+    console.log(element.regularPrice)
+    console.log(element.salePrice)
+    console.log(element.priceUpdateDate)
+
+    productIMGLink = element.image
+    productName = element.name
+    productPrice = element.regularPrice
+    productReview = element.customerReviewAverage
+    productLastPriceUpdate = element.priceUpdateDate
+
+    createTabs(newProducts, productIMGLink, productName, productPrice, productReview, productLastPriceUpdate)
+  });
+  
 }
